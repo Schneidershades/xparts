59d0364f0834d5683bfcf0c9e6759e0dc089b9da
Schneider <schneidershades@gmail.com>
2021-09-07 03:08:15 +0100

    assigned vendor xpart requests

--- app/Http/Controllers/Api/Vendor/UserXpartRequestController.php
+++ app/Http/Controllers/Api/Vendor/UserXpartRequestController.php
@@ -7,11 +7,79 @@ use Illuminate\Http\Request;
 
 class UserXpartRequestController extends Controller
 {
+    /**
+    * @OA\Get(
+    *      path="/api/v1/vendor/assigned/xpart-requests",
+    *      operationId="allAssignedXpartRequest",
+    *      tags={"Vendor"},
+    *      summary="allAssignedXpartRequest",
+    *      description="allAssignedXpartRequest",
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
     public function index()
     {
         return $this->showAll(auth()->user()->userXpartRequest);
     }
 
+    /**
+    * @OA\Delete(
+    *      path="/api/v1/vendor/assigned/xpart-requests/{id}",
+    *      operationId="deleteAssignedXpartRequest",
+    *      tags={"Vendor"},
+    *      summary="deleteAssignedXpartRequest",
+    *      description="deleteAssignedXpartRequest",
+    *      
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Assigned ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
     public function destroy(Request $request, $id)
     {
         auth()->user()->quotes->where('id', $id)->first()->delete();
