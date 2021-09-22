a703c287999ffb0f42e0f01921b5f0e68d1c8527
Schneider <schneidershades@gmail.com>
2021-09-22 16:23:15 +0100

    items

--- app/Http/Resources/Cart/CartResource.php
+++ app/Http/Resources/Cart/CartResource.php
@@ -18,7 +18,10 @@ class CartResource extends JsonResource
             
             'id' => $this->id,
             $this->mergeWhen($this->cartable_type == 'quotes', [
-                'title' => $this->cartable->xpartRequest->part->name .' '. $this->cartable->xpartRequest->vin->vin_number,
+                'title' => $this->cartable->xpartRequest->part->name,
+
+                'business' => $this->cartable->xpartRequest->vendor->business->first()->name,
+
                 'description' => $this->cartable->partGrade->name.' '.$this->cartable->brand .' '.$this->cartable->part_number,
             ]),
 
