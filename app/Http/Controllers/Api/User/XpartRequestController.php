84b29946965c514dbd4e36eed3a6595acd75db32
Rotibi Adedeji <dayjeerow7@gmail.com>
2021-09-23 21:20:50 +0100

    Add support for adding images on xpart_request

--- app/Http/Controllers/Api/User/XpartRequestController.php
+++ app/Http/Controllers/Api/User/XpartRequestController.php
@@ -10,82 +10,90 @@ use App\Http\Requests\User\XpartCreateFormRequest;
 class XpartRequestController extends Controller
 {
     /**
-    * @OA\Get(
-    *      path="/api/v1/xpart-requests",
-    *      operationId="allXpartRequest",
-    *      tags={"User"},
-    *      summary="allXpartRequest",
-    *      description="allXpartRequest",
-    *      @OA\Response(
-    *          response=200,
-    *          description="Successful signin",
-    *          @OA\MediaType(
-    *             mediaType="application/json",
-    *         ),
-    *       ),
-    *      @OA\Response(
-    *          response=400,
-    *          description="Bad Request"
-    *      ),
-    *      @OA\Response(
-    *          response=401,
-    *          description="unauthenticated",
-    *      ),
-    *      @OA\Response(
-    *          response=403,
-    *          description="Forbidden"
-    *      ),
-    *      security={ {"bearerAuth": {}} },
-    * )
-    */
+     * @OA\Get(
+     *      path="/api/v1/xpart-requests",
+     *      operationId="allXpartRequest",
+     *      tags={"User"},
+     *      summary="allXpartRequest",
+     *      description="allXpartRequest",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful signin",
+     *          @OA\MediaType(
+     *             mediaType="application/json",
+     *         ),
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      security={ {"bearerAuth": {}} },
+     * )
+     */
     public function index()
     {
         return $this->showAll(auth()->user()->xpartRequests);
     }
 
     /**
-    * @OA\Post(
-    *      path="/api/v1/xpart-requests",
-    *      operationId="postXpartRequests",
-    *      tags={"User"},
-    *      summary="postXpartRequests",
-    *      description="postXpartRequests",
-    *      @OA\RequestBody(
-    *          required=true,
-    *          @OA\JsonContent(ref="#/components/schemas/XpartCreateFormRequest")
-    *      ),
-    *      @OA\Response(
-    *          response=200,
-    *          description="Successful signin",
-    *          @OA\MediaType(
-    *             mediaType="application/json",
-    *         ),
-    *       ),
-    *      @OA\Response(
-    *          response=400,
-    *          description="Bad Request"
-    *      ),
-    *      @OA\Response(
-    *          response=401,
-    *          description="unauthenticated",
-    *      ),
-    *      @OA\Response(
-    *          response=403,
-    *          description="Forbidden"
-    *      ),
-    *      security={ {"bearerAuth": {}} },
-    * )
-    */
+     * @OA\Post(
+     *      path="/api/v1/xpart-requests",
+     *      operationId="postXpartRequests",
+     *      tags={"User"},
+     *      summary="postXpartRequests",
+     *      description="postXpartRequests",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/XpartCreateFormRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful signin",
+     *          @OA\MediaType(
+     *             mediaType="application/json",
+     *         ),
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      security={ {"bearerAuth": {}} },
+     * )
+     */
     public function store(XpartCreateFormRequest $request)
     {
         $users = User::select('id')->where('role', 'vendor')->get();
 
         $xpartRequest = auth()->user()->xpartRequests()->create($request->validated());
+        if ($request->has('images')) {
+            foreach ($request->images as $image) {
+                $path = $this->uploadImage($image, "xpart_requests");
+                $xpartRequest->images()->create([
+                    'file_path' => $path,
+                ]);
+            }
+        }
 
-        collect($users)->each(function ($user) use ($xpartRequest){
+        collect($users)->each(function ($user) use ($xpartRequest) {
             XpartRequestVendorWatch::insert([
-                'xpart_request_id'=> $xpartRequest->id,
-                'vendor_id'=> $user['id'],
+                'xpart_request_id' => $xpartRequest->id,
+                'vendor_id' => $user['id'],
             ]);
         });
 
@@ -93,13 +101,13 @@ class XpartRequestController extends Controller
     }
 
     /**
-    * @OA\Get(
-    *      path="/api/v1/xpart-requests/{id}",
-    *      operationId="showXpartRequests",
-    *      tags={"User"},
-    *      summary="showXpartRequests",
-    *      description="showXpartRequests",
-    *      
+     * @OA\Get(
+     *      path="/api/v1/xpart-requests/{id}",
+     *      operationId="showXpartRequests",
+     *      tags={"User"},
+     *      summary="showXpartRequests",
+     *      description="showXpartRequests",
+     *      
      *      @OA\Parameter(
      *          name="id",
      *          description="Xpart Request ID",
@@ -110,28 +118,28 @@ class XpartRequestController extends Controller
      *          )
      *      ),
      *      
-    *      @OA\Response(
-    *          response=200,
-    *          description="Successful signin",
-    *          @OA\MediaType(
-    *             mediaType="application/json",
-    *         ),
-    *       ),
-    *      @OA\Response(
-    *          response=400,
-    *          description="Bad Request"
-    *      ),
-    *      @OA\Response(
-    *          response=401,
-    *          description="unauthenticated",
-    *      ),
-    *      @OA\Response(
-    *          response=403,
-    *          description="Forbidden"
-    *      ),
-    *      security={ {"bearerAuth": {}} },
-    * )
-    */
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful signin",
+     *          @OA\MediaType(
+     *             mediaType="application/json",
+     *         ),
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      security={ {"bearerAuth": {}} },
+     * )
+     */
     public function show($id)
     {
         return $this->showOne(auth()->user()->xpartRequests->where('id', $id)->load('vendorQuotes')->first());
