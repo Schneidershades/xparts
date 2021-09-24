45120793543cc8a734ce4966563338c820c92723
Schneider <schneidershades@gmail.com>
2021-09-24 07:46:13 +0100

    item

--- app/Http/Requests/Address/AddressUpdateFormRequest.php
+++ app/Http/Requests/Address/AddressUpdateFormRequest.php
@@ -39,17 +39,6 @@ class AddressUpdateFormRequest extends FormRequest
 
     /**
      * @OA\Property(
-     *      title="User state",
-     *      description="state of the user",
-     *      example="No 4 Gang street"
-     * )
-     *
-     * @var string
-     */
-    public $state;
-
-    /**
-     * @OA\Property(
      *      title="User postal_code",
      *      description="postal_code of the user",
      *      example="900233"
