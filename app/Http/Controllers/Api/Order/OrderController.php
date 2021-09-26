7cafff5349835a90cc69e6a8cf15b8d9a99e78c1
Rotibi Adedeji <dayjeerow7@gmail.com>
2021-09-26 13:07:33 +0100

    Refactor for support for multiple payment methods

--- app/Http/Controllers/Api/Order/OrderController.php
+++ app/Http/Controllers/Api/Order/OrderController.php
@@ -198,41 +198,16 @@ class OrderController extends Controller
     public function update(OrderUpdateFormRequest $request)
     {
         $order = Order::where('id', $request['order_id'])->first();
-        
-        $paystack = new Paystack;
-        $tx = $paystack->verify($request['payment_reference']);
 
-        if ($tx->status && $tx->data->status == "success") {
-            $txData = $tx->data;
-            $order->update([
-                'amount_paid' => $txData->amount/100,
-                // 'currency_id' => '',
-                'currency' => $txData->currency,
-                'payment_method' => $txData->channel,
-                'payment_gateway' => $request['payment_gateway'],
-                // 'payment_gateway_charged_percentage' => '',
-                // 'payment_gateway_expected_charged_percentage' => '',
-                'payment_reference' => $request['payment_reference'],
-                'payment_gateway_charge' => $txData->fees,
-                // 'payment_gateway_remittance' => '',
-                // 'payment_code' => '',
-                'payment_message' => $tx->message,
-                'payment_status' => $txData->status,
-                // 'platform_initiated' => '',
-                'transaction_initiated_date' => $txData->transaction_date,
-                'transaction_initiated_time' => $txData->transaction_date,
-                'date_time_paid' => now(),
-            ]);
+        $paystack = new Paystack;
+        [$status, $data] = $paystack->verify($request['payment_reference'], "order");
+
+        if ($status == "success") {
+            $order->update($data);
 
             return $this->showOne($order);
         } else {
-            return $this->errorResponse($tx->message, 400);
+            return $this->errorResponse($data, 400);
         }
-
-        // $payment_status = match($request->payment_gateway){
-        //     'paystack' => 1,
-        //     'flutterwave' => 2,
-        // };
-        // return $this->showOne(auth()->user()->cart->where('id', $id)->first()->update($request->validated()));
     }
 }
--- app/Traits/Payment/Paystack.php
+++ app/Traits/Payment/Paystack.php
@@ -24,12 +24,44 @@ class Paystack
         }
     }
 
-    public function verify($reference)
+    public function verify($reference, $type = "order")
     {
         $response = Http::withToken($this->secretKey)
             ->asJson()
             ->get($this->baseUrl . "/transaction/verify/$reference");
 
-        return json_decode($response->body());
+        $tx = json_decode($response->body());
+        if ($tx->status && $tx->data->status == "success") {
+            $txData = $tx->data;
+            switch ($type) {
+                case 'order':
+                    $data = [
+                        'amount_paid' => $txData->amount / 100,
+                        // 'currency_id' => '',
+                        'currency' => $txData->currency,
+                        'payment_method' => $txData->channel,
+                        'payment_gateway' => "paystack",
+                        // 'payment_gateway_charged_percentage' => '',
+                        // 'payment_gateway_expected_charged_percentage' => '',
+                        'payment_reference' => $reference,
+                        'payment_gateway_charge' => $txData->fees,
+                        // 'payment_gateway_remittance' => '',
+                        // 'payment_code' => '',
+                        'payment_message' => $tx->message,
+                        'payment_status' => $txData->status,
+                        // 'platform_initiated' => '',
+                        'transaction_initiated_date' => $txData->transaction_date,
+                        'transaction_initiated_time' => $txData->transaction_date,
+                        'date_time_paid' => now(),
+                    ];
+                    break;
+                default:
+                    $data = [];
+                    break;
+            }
+            return ["success", $data];
+        } else {
+            return ["error", $tx->message];
+        }
     }
 }
