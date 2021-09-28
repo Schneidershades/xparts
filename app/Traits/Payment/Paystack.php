e00de74e8bec6330d4cbfb6b315df64823fdd4de
Schneider <schneidershades@gmail.com>
2021-09-28 17:23:21 +0100

    items

--- app/Traits/Payment/Paystack.php
+++ app/Traits/Payment/Paystack.php
@@ -47,27 +47,27 @@ class Paystack
         $tx = json_decode($response, true);
 
         // $tx = json_decode($response->body());
-        if ($tx->status && $tx->data->status == "success") {
-            $txData = $tx->data;
+        if ($tx['status'] && $tx['data']['status'] == "success") {
+            $txData = $tx['data'];
             switch ($type) {
                 case 'order':
                     $data = [
-                        'amount_paid' => $txData->amount / 100,
+                        'amount_paid' => $txData['amount'] / 100,
                         // 'currency_id' => '',
-                        'currency' => $txData->currency,
-                        'payment_method' => $txData->channel,
+                        'currency' => $txData['currency'],
+                        'payment_method' => $txData['channel'],
                         'payment_gateway' => "paystack",
                         // 'payment_gateway_charged_percentage' => '',
                         // 'payment_gateway_expected_charged_percentage' => '',
                         'payment_reference' => $reference,
-                        'payment_gateway_charge' => $txData->fees,
+                        'payment_gateway_charge' => $txData['fees'],
                         // 'payment_gateway_remittance' => '',
                         // 'payment_code' => '',
-                        'payment_message' => $tx->message,
-                        'payment_status' => $txData->status,
+                        'payment_message' => $tx['message'],
+                        'payment_status' => $txData['status'],
                         // 'platform_initiated' => '',
-                        'transaction_initiated_date' => $txData->transaction_date,
-                        'transaction_initiated_time' => $txData->transaction_date,
+                        'transaction_initiated_date' => $txData['transaction_date'],
+                        'transaction_initiated_time' => $txData['transaction_date'],
                         'date_time_paid' => now(),
                     ];
                     break;
@@ -77,7 +77,7 @@ class Paystack
             }
             return ["success", $data];
         } else {
-            return ["error", $tx->message];
+            return ["error", $tx['message']];
         }
     }
 }
