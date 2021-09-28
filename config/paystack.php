58263b3180aeb2d4e7d497a2e06c6f8f7e747af4
Schneider <schneidershades@gmail.com>
2021-09-28 16:58:51 +0100

    ssi

--- config/paystack.php
+++ config/paystack.php
@@ -3,6 +3,6 @@
 return [
     'url' => env('PAYSTACK_PAYMENT_URL'),
     'public_key' => env('PAYSTACK_ENV') == 'test' ? env('PAYSTACK_TEST_PUBLIC_KEY') : env('PAYSTACK_LIVE_PUBLIC_KEY'),
-    'secret_key' => env('PAYSTACK_ENV') == 'test' ? env('PAYSTACK__TEST_SECRET_KEY') : env('PAYSTACK_LIVE_SECRET_KEY'),
+    'secret_key' => env('PAYSTACK_ENV') == 'test' ? env('PAYSTACK_TEST_SECRET_KEY') : env('PAYSTACK_LIVE_SECRET_KEY'),
 ];
 
