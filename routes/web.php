10dee99e9f22541a47ecc8c573a6d047308a6178
Rotibi Adedeji <dayjeerow7@gmail.com>
2021-09-29 21:49:41 +0100

    Setup test broadcasting event

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('send', function () {
+//     broadcast(new \App\Events\TestEvent());
+//     return "OK";
+// });
