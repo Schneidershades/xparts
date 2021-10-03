d9cec2ed089bb24125660a066ad20f3134cec03c
Schneider <schneidershades@gmail.com>
2021-10-03 12:09:09 +0100

    ssh

--- app/Http/Controllers/Api/Shared/Wallet/FundController.php
+++ app/Http/Controllers/Api/Shared/Wallet/FundController.php
@@ -172,6 +172,8 @@ class FundController extends Controller
             $paystack = new Paystack;
             [$status, $data] = $paystack->verify($request['payment_reference'], "order");
 
+            return $status;
+
             if ($status != "success") {
                 return $this->errorResponse($data, 400);
             } 
