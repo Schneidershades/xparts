f8a8a11656f09b2a46d5cb1673f818692c53635f
Schneider <schneidershades@gmail.com>
2021-10-06 00:41:25 +0100

    debit and credit from payments

--- app/Http/Controllers/Api/Order/OrderController.php
+++ app/Http/Controllers/Api/Order/OrderController.php
@@ -319,6 +319,8 @@ class OrderController extends Controller
         $vendor->balance += $item->price;
         $vendor->save();
 
+        dd($vendor);
+
         WalletTransaction::create([
             'receipt_number' => $order->receipt_number,
             'title' => $order->title,
