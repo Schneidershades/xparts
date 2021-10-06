25138406a4841af02ac9b6119c1b49eac77dae72
Schneider <schneidershades@gmail.com>
2021-10-06 01:28:49 +0100

    min

--- app/Http/Controllers/Api/Order/OrderController.php
+++ app/Http/Controllers/Api/Order/OrderController.php
@@ -319,8 +319,6 @@ class OrderController extends Controller
         $vendor->balance = $vendor->balance  + $item->price;
         $vendor->save();
 
-        dd($vendor, $vendor->user);
-
         WalletTransaction::create([
             'receipt_number' => $order->receipt_number,
             'title' => $order->title,
