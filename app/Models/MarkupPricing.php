e186461a90d783d711b1a37e7798d789a8d22628
Schneider <schneidershades@gmail.com>
2021-10-16 15:14:36 +0100

    markup pricing

--- app/Models/MarkupPricing.php
+++ app/Models/MarkupPricing.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class MarkupPricing extends Model
 {
     use HasFactory;
+    
+    public $oneItem = MarkupPricingResource::class;
+    public $allItems = MarkupPricingCollection::class;
 
     protected $fillable = ['min_value', 'max_value', 'percentage', 'active', 'type'];
 }
