8a07331a74ce70f7eb8c83edeed5a9dc45a89ea2
Schneider <schneidershades@gmail.com>
2021-10-18 13:54:03 +0100

    orders

--- app/Http/Controllers/Api/User/OrderController.php
+++ app/Http/Controllers/Api/User/OrderController.php
@@ -38,7 +38,7 @@ class OrderController extends Controller
     */
     public function index()
     {
-        return $this->showAll(auth()->user()->orders);
+        return $this->showAll(auth()->user()->orders->latest()->get());
     }
 
     /**
