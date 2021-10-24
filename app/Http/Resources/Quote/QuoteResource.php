500c246cd16580daaee5815bbf11dc0aef913dd7
Schneider <schneidershades@gmail.com>
2021-10-24 15:16:00 +0100

    vin checker items models

--- app/Http/Resources/Quote/QuoteResource.php
+++ app/Http/Resources/Quote/QuoteResource.php
@@ -26,7 +26,7 @@ class QuoteResource extends JsonResource
             'quantity' => $this->quantity,
             'part_number' => $this->part_number,
             'part_warranty' => $this->part_warranty,
-            'actual_price' => $this->markup_price,
+            'actual_price' => $this->getOriginal('price'),
             'price' => $this->price,
             'description' => $this->description,
             'status' => $this->status,
