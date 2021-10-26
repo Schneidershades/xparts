08d63f670cd5650b6f5a069cdd20e710fc1af0f7
Schneider <schneidershades@gmail.com>
2021-10-26 06:08:16 +0100

    ssr

--- app/Http/Resources/Cart/CartCollection.php
+++ app/Http/Resources/Cart/CartCollection.php
@@ -21,7 +21,7 @@ class CartCollection extends ResourceCollection
             'cart' => [
 
                 'total' => $this->collection->sum(function ($cart) {
-                    return $cart->cartable->markup_price ? $cart->cartable->markup_price : $cart->cartable->price * $cart->cartable->quantity;
+                    return $cart->cartable->quantity;
                 }),
 
                 'subtotal' => $this->collection->sum(function ($cart) {
