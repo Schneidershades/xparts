e20401cfd11aa62858456ba13da435cc348b0c20
Schneider <schneidershades@gmail.com>
2021-10-31 00:30:55 +0100

    xparts update

---
+++ app/Http/Controllers/Api/Admin/VinPartsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Models\Vin;
+use App\Models\Part;
+use App\Models\XpartRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\VinPartsFormRequest;
+
+class VinPartsController extends Controller
+{
+     /**
+    * @OA\Post(
+    *      path="/api/v1/admin/vin-parts",
+    *      operationId="postVinPart",
+    *      tags={"Admin"},
+    *      summary="postVinPart",
+    *      description="postVinPart",
+    *      @OA\RequestBody(
+    *          required=true,
+    *          @OA\JsonContent(ref="#/components/schemas/VinPartsFormRequest")
+    *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
+    public function store(VinPartsFormRequest $request)
+    {
+        $vin = Vin::where('id', $request['vin_id'])->first();
+        $part = Part::where('id', $request['part_id'])->first();
+        $xpartRequest = XpartRequest::where('id', $request['xpart_request_id'])->first();
+        
+        if($vin){
+            $vin = $this->contentAndDbIntersection($request->all(), $vin, [], [
+                'admin_attention' => $request['make_vin_active']
+            ]);
+            $vin->save();
+        }
+        
+        if($part){
+            $part = $this->contentAndDbIntersection($request->all(), $part, [], [
+                'admin_attention' => $request['make_part_active']
+            ]);
+            $part->save();
+        }
+
+        if($request['make_xpart_request_active'] == true){
+            $xpartRequest->status = 'active';
+            $xpartRequest->save();
+        }
+    }
+}
---
+++ app/Http/Requests/Admin/VinPartsFormRequest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VinPartsFormRequest extends FormRequest
+{
+    /**
+     * @OA\Property(
+     *      title="vinid",
+     *      description="vin id",
+     *      example="1"
+     * )
+     *
+     * @var int
+     */
+    private $vin_id;
+
+    /**
+     * @OA\Property(
+     *      title="part id",
+     *      description="part id",
+     *      example="1"
+     * )
+     *
+     * @var int
+     */
+    private $part_id;
+
+    /**
+     * @OA\Property(
+     *      title="xpart request id",
+     *      description="xpart request id",
+     *      example="1"
+     * )
+     *
+     * @var int
+     */
+    private $xpart_request_id;
+
+
+    /**
+     * @OA\Property(
+     *      title="make",
+     *      description="vin make",
+     *      example="toyota"
+     * )
+     *
+     * @var string
+     */
+    private $make;
+
+
+
+    /**
+     * @OA\Property(
+     *      title="model",
+     *      description="vin model",
+     *      example="toyota"
+     * )
+     *
+     * @var string
+     */
+    private $model;
+
+
+    /**
+     * @OA\Property(
+     *      title="model_year",
+     *      description="vin model_year",
+     *      example="toyota"
+     * )
+     *
+     * @var string
+     */
+    private $model_year;
+
+    /**
+     * @OA\Property(
+     *      title="make_part_active",
+     *      description="make_part_active",
+     *      example="true"
+     * )
+     *
+     * @var boolean
+     */
+    private $make_part_active;
+
+
+    /**
+     * @OA\Property(
+     *      title="make_vin_active",
+     *      description="make_vin_active",
+     *      example="true"
+     * )
+     *
+     * @var boolean
+     */
+    private $make_vin_active;
+
+    /**
+     * @OA\Property(
+     *      title="make_xpart_request_active",
+     *      description="make_xpart_request_active",
+     *      example="true"
+     * )
+     *
+     * @var boolean
+     */
+    private $make_xpart_request_active;
+
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'vin_id' => 'required|integer|exists:vins,id',
+            'xpart_request_id' => 'required|integer|exists:xpart_requests,id',
+            'part_id' => 'required|integer|exists:parts,id',
+            'make' => 'nullable',
+            'model' => 'nullable',
+            'model_year' => 'nullable',
+            'make_part_active' => 'required|boolean',
+            'make_vin_active' => 'required|boolean',
+            'make_xpart_request_active' => 'required|boolean',
+        ];
+    }
+}
