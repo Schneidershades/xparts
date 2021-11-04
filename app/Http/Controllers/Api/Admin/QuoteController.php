3d3fbb7bc8b2bc760e8b91099dff57a6ab5a2274
Schneider <schneidershades@gmail.com>
2021-11-04 01:11:58 +0100

    git

--- app/Http/Controllers/Api/Admin/QuoteController.php
+++ app/Http/Controllers/Api/Admin/QuoteController.php
@@ -118,7 +118,7 @@ class QuoteController extends Controller
 
     public function findOrderItemsForQuotesSelected($order, $quote)
     {
-        $item = OrderItem::where('receipt_number', $$order->receipt_number)->where('itemable_id', $quote->id)->where('itemable_type', 'quotes')->first();
+        $item = OrderItem::where('receipt_number', $order->receipt_number)->where('itemable_id', $quote->id)->where('itemable_type', 'quotes')->first();
         return $item;
     }
 
