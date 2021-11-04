b8cd8206fb59e7c104e5fbd08e3ebad95ba08398
Schneider <schneidershades@gmail.com>
2021-11-04 04:18:00 +0100

    change status

--- app/Http/Resources/Xpart/XpartRequestResource.php
+++ app/Http/Resources/Xpart/XpartRequestResource.php
@@ -26,27 +26,7 @@ class XpartRequestResource extends JsonResource
             
             'receipt_number' => $this->receipt_number,
             
-            // 'status' => $this->status,
-
-            $this->mergeWhen($this->status =='expired' , [
-                'status' => 'expired',
-            ]),
-
-            $this->mergeWhen($this->status =='ordered' , [
-                'status' => 'ordered',
-            ]),
-
-            $this->mergeWhen($this->status =='xparts2user' || $this->status =='vendor2xparts' && auth()->user()->role == 'user', [
-                'status' => 'paid',
-            ]),
-
-            $this->mergeWhen($this->status == 'delivered' || $this->status == 'awaiting' || $this->status == 'active' || $this->status == 'paid' && auth()->user()->role == 'user', [
-                'status' => $this->status,
-            ]),
-
-            $this->mergeWhen(auth()->user()->role == 'admin', [
-                'status' => $this->status,
-            ]),
+            'status' => $this->status,
         ];
     }
 }
