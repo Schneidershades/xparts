bef1cf22160a9abfb718e0b8139267c3a7ccc85d
Schneider <schneidershades@gmail.com>
2021-11-06 12:14:52 +0100

    stock

--- app/Http/Controllers/Api/Order/OrderController.php
+++ app/Http/Controllers/Api/Order/OrderController.php
@@ -428,7 +428,7 @@ class OrderController extends Controller
 
         if($orderItemDetails->itemable_type == 'quotes'){
             $title = 'Receiving '.  $orderItemDetails->itemable_type . ' transaction payment';
-            $title = 'Receiving '.  $orderItemDetails->itemable_type . ' transaction payment';
+            $details = 'Receiving '.  $orderItemDetails->itemable_type . ' transaction payment';
         }
 
         $newOrder = $vendorBalance->user->orders()->create([
