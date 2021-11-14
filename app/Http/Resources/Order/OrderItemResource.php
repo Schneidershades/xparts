3f290ddf81272fb8078190b9b449f76b7381ec8c
Schneider <schneidershades@gmail.com>
2021-11-14 09:01:03 +0100

    push

--- app/Http/Resources/Order/OrderItemResource.php
+++ app/Http/Resources/Order/OrderItemResource.php
@@ -16,30 +16,31 @@ class OrderItemResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            $this->mergeWhen($this->itemable_type == 'quotes', [
-                
+            $this->mergeWhen($this->itemable_type == 'quotes' && $this->itemable_id, [
+
                 'title' => $this->itemable_type ? $this->itemable->xpartRequest->part->name : 'N/A',
                 'grade' => $this->itemable_type ?  $this->itemable->partGrade->name : 'N/A',
                 'brand' => $this->itemable_type ? $this->itemable->brand : 'N/A',
                 'part_number' => $this->itemable_type ? $this->itemable->part_number : 'N/A',
                 'vendor_id' => $this->itemable_type ? $this->itemable->vendor_id : 'N/A',
 
-                'measurement' => $this->itemable->measurement ? $this->itemable->measurement : null,
-                'available_stock' => $this->itemable->quantity ? $this->itemable->quantity : null,
-                'make' => $this->itemable->xpartRequest ? $this->itemable->xpartRequest->vin->make : null,
-                'model' => $this->itemable->xpartRequest ?  $this->itemable->xpartRequest->vin->model : null,
-                'year' => $this->itemable->xpartRequest ?  $this->itemable->xpartRequest->vin->model_year : null,
+                'measurement' => $this->itemable_type ? $this->itemable->measurement : null,
+                'available_stock' => $this->itemable_type ? $this->itemable->quantity : null,
+                'make' => $this->itemable_type ? $this->itemable->xpartRequest->vin->make : null,
+                'model' => $this->itemable_type ?  $this->itemable->xpartRequest->vin->model : null,
+                'year' => $this->itemable_type ?  $this->itemable->xpartRequest->vin->model_year : null,
                 
-                'status' => $this->itemable ? $this->itemable->status : 'N/A',
+                'status' => $this->itemable_type ? $this->itemable->status : 'N/A',
             ]),
             
             'cartable_type' => $this->cartable_type,
             'cartable_id' => $this->cartable_id,
 
             'category' => $this->cartable_type,
-            'price' => $this->itemable ? $this->itemable->price : 'N/A',
+            'price' => $this->itemable_type ? $this->itemable->price : 'N/A',
             'quantity' => $this->quantity,
-            'total' => $this->itemable ? $this->itemable->price : 0 * $this->quantity,
+            'total' => $this->itemable_type ? $this->itemable->price : 0 * $this->quantity,
         ];
     }
+    
 }
