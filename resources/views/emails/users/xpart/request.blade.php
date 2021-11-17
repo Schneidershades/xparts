b91717674faee31972c98a2a14c2387bc892742d
Schneider <schneidershades@gmail.com>
2021-11-17 13:46:27 +0100

    schn

--- resources/views/emails/users/xpart/request.blade.php
+++ resources/views/emails/users/xpart/request.blade.php
@@ -2,9 +2,9 @@
 # Hello {{ $user['name'] }}
 
 
-@if($xp->part->status != 'active')
+@if($xp->part->status == 'active')
 A new xpart request for part vehicle part **{{ $xp->part->name }}** - **{{ $xp->vin->vehicle_name }}** has been created.
-@elseif($xp->part->status != 'awaiting')
+@elseif($xp->part->status == 'awaiting')
 A new xpart request for part vehicle part **{{ $xp->part->name }}** - **{{ $xp->vin->vehicle_name }}** has been created and is awaiting approval.
 @endif
 
