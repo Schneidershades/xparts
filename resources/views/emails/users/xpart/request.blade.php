e53acb7be41e10e371812a6c73a920818695b6b7
Schneider <schneidershades@gmail.com>
2021-11-17 13:44:39 +0100

    schn

--- resources/views/emails/users/xpart/request.blade.php
+++ resources/views/emails/users/xpart/request.blade.php
@@ -1,7 +1,12 @@
 @component('mail::message')
 # Hello {{ $user['name'] }}
 
+
+@if($xp->part->status != 'active')
 A new xpart request for part vehicle part **{{ $xp->part->name }}** - **{{ $xp->vin->vehicle_name }}** has been created.
+@elseif($xp->part->status != 'awaiting')
+A new xpart request for part vehicle part **{{ $xp->part->name }}** - **{{ $xp->vin->vehicle_name }}** has been created and is awaiting approval.
+@endif
 
 @component('mail::button', ['url' => $link])
 View xpart request
