9b40d319289ad7e7b8bdd16f92cc97cd10cf4bdb
Schneider <schneidershades@gmail.com>
2021-11-19 19:06:30 +0100

    numbers

--- app/Http/Resources/Order/OrderItemResource.php
+++ app/Http/Resources/Order/OrderItemResource.php
@@ -6,20 +6,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderItemResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
+
             $this->mergeWhen($this->itemable_type == 'quotes' && $this->itemable_id, [
 
-                'title' => $this->itemable_type ? $this->itemable->xpartRequest->part->name : 'N/A',
-                'grade' => $this->itemable_type ?  $this->itemable->partGrade->name : 'N/A',
+                'title' => $this->itemable->xpartRequest ? $this->itemable->xpartRequest->part->name : 'N/A',
+                'grade' => $this->itemable->partGrade ?  $this->itemable->partGrade->name : 'N/A',
                 'brand' => $this->itemable_type ? $this->itemable->brand : 'N/A',
                 'part_number' => $this->itemable_type ? $this->itemable->part_number : 'N/A',
                 'vendor_id' => $this->itemable_type ? $this->itemable->vendor_id : 'N/A',
