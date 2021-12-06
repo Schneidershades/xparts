6147e58bfbeebf798199dfe9a72c91a1cfe014d8
Schneider <schneidershades@gmail.com>
2021-12-06 13:55:32 +0100

    page seeder

---
+++ app/Http/Controllers/Api/Shared/PageController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api\Shared;
+
+use App\Models\Page;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    /**
+    * @OA\Get(
+    *      path="/api/v1/shared/pages/{slug}",
+    *      operationId="showPublicPage",
+    *      tags={"Shared"},
+    *      summary="Show PublicPage",
+    *      description="Show PublicPage",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="showPublicPage Slug",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
+    public function show($slug)
+    {
+        return $this->showOne(Page::where('slug', $slug)->first());
+    }
+}
