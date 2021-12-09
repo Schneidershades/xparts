dadf792ea7dd17c9d5484789347f0cf0e8c6a8e4
Schneider <schneidershades@gmail.com>
2021-12-09 22:23:11 +0100

    send email

--- resources/views/emails/users/xpart/password-changed.blade.php
+++ resources/views/emails/users/xpart/password-changed.blade.php
@@ -3,7 +3,7 @@
 
 Your password was changed successfully. You can now login with the following details:
 
-<h3>{{$passwordDetails}}</h3>
+<h3>New Password : {{$passwordDetails}}</h3>
 
 Thanks,<br>
 {{ config('app.name') }}
