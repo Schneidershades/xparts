1b69eb4daaea1bf4667409567662d2a983051f73
Schneider <schneidershades@gmail.com>
2021-12-17 05:11:45 +0100

    optional

--- app/Http/Resources/Order/OrderItemResource.php
+++ app/Http/Resources/Order/OrderItemResource.php
@@ -14,6 +14,18 @@ class OrderItemResource extends JsonResource
             // $this->mergeWhen($this->itemable == 'quotes', [
                 'title' => $this->itemable?->xpartRequest?->part?->name,
                 'grade' => $this->itemable?->partGrade?->name,
+                'brand' => $this->itemable?->brand,
+                'part_number' => $this->itemable?->part_number,
+                'vendor_id' => $this->itemable?->vendor_id,
+                'measurement' => $this->itemable?->measurement,
+                'available_stock' => $this->itemable?->quantity,
+                'make' => $this->itemable?->xpartRequest?->vin?->make,
+                'model' => $this->itemable?->xpartRequest?->vin?->model,
+                'year' => $this->itemable?->xpartRequest?->vin?->model_year,
+                'status' => $this->itemable?->status,
+                'price' => $this->itemable?->markup_price,
+                'total' => $this->itemable ? $this->itemable?->markup_price : 0 * $this->quantity,
+                
                 // 'brand' => $this->itemable ? $this->itemable->brand : 'N/A',
                 // 'part_number' => $this->itemable ? $this->itemable->part_number : 'N/A',
                 // 'vendor_id' => $this->itemable ? $this->itemable->vendor_id : 'N/A',
