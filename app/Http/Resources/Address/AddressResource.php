d0b6636e77efb833c492f47ef598e65dfe898196
Schneider <schneidershades@gmail.com>
2021-12-21 12:32:54 +0100

    delivery

--- app/Http/Resources/Address/AddressResource.php
+++ app/Http/Resources/Address/AddressResource.php
@@ -34,9 +34,9 @@ class AddressResource extends JsonResource
                 'delivery_rate' => $this->state->stateDeliveryRate?->amount,
             ]),
 
-            // $this->mergeWhen($this->stateDeliveryRate == null && $this->cityDeliveryRate != null, [
-            //     'delivery_rate' => $this->city->cityDeliveryRate?->amount,
-            // ]),
+            $this->mergeWhen($this->stateDeliveryRate == null || $this->cityDeliveryRate != null, [
+                'delivery_rate' => $this->city->cityDeliveryRate?->amount,
+            ]),
 
             // $this->mergeWhen($this->stateDeliveryRate == null && $this->cityDeliveryRate == null, [
             //     'delivery_rate' => $this->city->flatRate()?->amount,
