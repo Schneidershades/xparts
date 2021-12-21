d02e7450738111b9ddfafcffa92cd0a747fccc44
Schneider <schneidershades@gmail.com>
2021-12-21 12:14:38 +0100

    delivery

--- app/Http/Resources/Address/AddressResource.php
+++ app/Http/Resources/Address/AddressResource.php
@@ -26,21 +26,21 @@ class AddressResource extends JsonResource
             'postal_code' => $this->postal_code,
             'default' => $this->primary_address ? true : false,
 
-            // $this->mergeWhen($this->stateDeliveryRate && $this->cityDeliveryRate, [
-                'delivery_rate' => $this->city->cityDeliveryRate,
-            // ]),
+            $this->mergeWhen($this->stateDeliveryRate && $this->cityDeliveryRate, [
+                'delivery_rate' => $this->city->cityDeliveryRate?->amount,
+            ]),
 
-            // $this->mergeWhen($this->stateDeliveryRate != null && $this->cityDeliveryRate == null, [
-            //     'delivery_rate' => $this->state->stateDeliveryRate->amount,
-            // ]),
+            $this->mergeWhen($this->stateDeliveryRate != null && $this->cityDeliveryRate == null, [
+                'delivery_rate' => $this->state->stateDeliveryRate?->amount,
+            ]),
 
-            // $this->mergeWhen($this->stateDeliveryRate == null && $this->cityDeliveryRate != null, [
-            //     'delivery_rate' => $this->city->cityDeliveryRate->amount,
-            // ]),
+            $this->mergeWhen($this->stateDeliveryRate == null && $this->cityDeliveryRate != null, [
+                'delivery_rate' => $this->city->cityDeliveryRate?->amount,
+            ]),
 
-            // $this->mergeWhen($this->stateDeliveryRate == null && $this->cityDeliveryRate == null, [
-            //     'delivery_rate' => $this->city->cityDeliveryRate->amount,
-            // ]),
+            $this->mergeWhen($this->stateDeliveryRate == null && $this->cityDeliveryRate == null, [
+                'delivery_rate' => $this->city->cityDeliveryRate?->amount,
+            ]),
 
         ];
     }
