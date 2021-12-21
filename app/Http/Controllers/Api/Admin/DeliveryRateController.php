ef8c88e52a0a782043c28c02ac3bd289d25da5bb
Schneider <schneidershades@gmail.com>
2021-12-21 12:29:19 +0100

    delivery

--- app/Http/Controllers/Api/Admin/DeliveryRateController.php
+++ app/Http/Controllers/Api/Admin/DeliveryRateController.php
@@ -78,10 +78,10 @@ class DeliveryRateController extends Controller
     */
     public function store(DeliveryRateCreateFormRequest $request)
     {
-        $count = DeliveryRate::all()->count();
-        if($count > 1){
-            return $this->errorResponse('You cannot create any further. Kindly edit the one available', 409);
-        }
+        // $count = DeliveryRate::all()->count();
+        // if($count > 1){
+        //     return $this->errorResponse('You cannot create any further. Kindly edit the one available', 409);
+        // }
         return $this->showOne(DeliveryRate::create($request->validated()));
     }
 
