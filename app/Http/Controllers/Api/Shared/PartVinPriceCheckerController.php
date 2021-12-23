16ead8033974e1a0629a18a36914502aff805c9c
Schneider <schneidershades@gmail.com>
2021-12-23 17:38:11 +0100

    git push

--- app/Http/Controllers/Api/Shared/PartVinPriceCheckerController.php
+++ app/Http/Controllers/Api/Shared/PartVinPriceCheckerController.php
@@ -76,14 +76,14 @@ class PartVinPriceCheckerController extends Controller
 
             if(count($quote_prices) == 1){
 
-                $message .= 'Price range for '. $grade->name .' (₦' . number_format($quote_prices[0], 2, '.', ',') .') \n';
+                $message .= 'Price range for '. $grade->name .' (₦' . number_format($quote_prices[0], 2, '.', ',') .') <br>';
             }
 
             if(count($quote_prices) > 1){
                 $minPrice = min($quote_prices);
                 $maxPrice = max($quote_prices);
 
-                $message .= 'Price ranges from '. $grade->name .' (₦' . number_format($minPrice, 2, '.', ',') . ' - ₦' . number_format($maxPrice, 2, '.', ',') .') \n';
+                $message .= 'Price ranges from '. $grade->name .' (₦' . number_format($minPrice, 2, '.', ',') . ' - ₦' . number_format($maxPrice, 2, '.', ',') .') <br>';
             }    
         }
 
