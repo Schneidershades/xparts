47ceda99f4306291c31ef9fd47fe7626251018d5
Schneider <schneidershades@gmail.com>
2022-01-06 17:55:51 +0100

    git push

--- app/Http/Resources/Address/AddressResource.php
+++ app/Http/Resources/Address/AddressResource.php
@@ -19,30 +19,23 @@ class AddressResource extends JsonResource
             'country' => $this->country ? $this->country->name : 'N/A',
             'postal_code' => $this->postal_code,
             'default' => $this->primary_address ? true : false,
-            // 'delivery_rate' => $this->city->flatRate()?->amount,
 
-            // if address has any delivery rate on city leave country & state
-
-            // if address has any delivery rate no city has state leave country
-
-            // if address has any delivery rate no city has state leave country
-
-
-            $this->mergeWhen($this->state->rate != null && $this->city->rate != null, [
+            $this->mergeWhen($this->city->rate != null, [
                 'delivery_rate' => $this->city->rate?->amount,
             ]),
 
-            // $this->mergeWhen($this->state->rate != null || $this->city->rate == null, [
-            //     'delivery_rate' => $this->state->rate?->amount,
-            // ]),
+            $this->mergeWhen($this->city->rate == null && $this->state->rate, [
+                'delivery_rate' => $this->state->rate?->amount,
+            ]),
 
-            // $this->mergeWhen($this->state->rate == null || $this->city->rate != null, [
-            //     'delivery_rate' => $this->city->rate?->amount,
-            // ]),
+            $this->mergeWhen($this->city->rate == null && $this->state->rate == null && $this->country->rate != null, [
+                'delivery_rate' => $this->country->rate?->amount,
+            ]),
 
-            // $this->mergeWhen($this->state->rate == null && $this->city->rate == null && $this->country->rate == null , [
-            //     'delivery_rate' => $this->city->flatRate()?->amount,
-            // ]),
+            $this->mergeWhen($this->city->rate == null && $this->state->rate == null && $this->country->rate == null, [
+                'delivery_rate' => $this->city->flatRate()?->amount,
+            ]),
+
 
         ];
     }
