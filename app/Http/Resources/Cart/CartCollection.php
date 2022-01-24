da3dd78d8a34bf7c29c607efd6923babc300c665
Schneider <schneidershades@gmail.com>
2022-01-24 14:36:45 +0100

    cece

--- app/Http/Resources/Cart/CartCollection.php
+++ app/Http/Resources/Cart/CartCollection.php
@@ -21,7 +21,7 @@ class CartCollection extends ResourceCollection
             
             'cart' => [
 
-                'total' => $total + $deliveryFee,
+                'total' => $total ,
 
                 'subtotal' => $total + $deliveryFee,
 
